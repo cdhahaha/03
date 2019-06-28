6592b7cae11ae5a7250a39cfb6902ca85732a10e
chend <cdhahaha@163.com>
2019-06-28 17:35:04 +0800

    初始化

---
+++ src/views/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="homeWrap">
+    <el-image :src="homePic" class="bannerPic"></el-image>
+  </div>
+</template>
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      homePic: require('@/assets/images/1.jpg')
+    }
+  }
+}
+</script>
+<style lang="less">
+  .homeWrap {
+    .bannerPic {
+    }
+  }
+</style>
